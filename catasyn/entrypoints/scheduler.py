96a9691a1ed9670410960fb7e85602430e4932f9
Antonio <antonio.one@pm.me>
2021-03-25 15:16:47 +0000

    changing logging exception

--- catasyn/entrypoints/scheduler.py
+++ catasyn/entrypoints/scheduler.py
@@ -66,7 +66,8 @@ def synchronise_all_topics():
         mappings = requests.get(url=url)
         mappings.raise_for_status()
     except requests.exceptions.ConnectionError:
-        logging.exception()
+        message = ""
+        logging.exception(message)
         return
     else:
         message = f"{url} returned status {mappings.status_code}"
